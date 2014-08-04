203f1e5a75cc008e4b6cff6731ab2c88c542d2cc
robin <robgro@telia.com>
2014-08-04 07:41:21 +0200

    Restructured some php files and removed -flag usage since it didn't really make any sence

--- app/Constants.php
+++ app/Constants.php
@@ -10,9 +10,9 @@ class Constants{
     public static $TMDB_KEY = "TMDB_KEY";
     public static $CONFIG_FILENAME = "config.ini";
     public static $HELP_TEXT =
-"Usage: easylib -help => Print this help text
-       easylib -scan => Scans the filesystem for new media
-       easylib -config => Gets or sets properties from the config file
+"Usage: easylib help => Print this help text
+       easylib scan => Scans the filesystem for new media
+       easylib config => Gets or sets properties from the config file
 
 ";
 
